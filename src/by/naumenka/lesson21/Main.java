1c9addc88034d9e1a0a0fe518d486d6f1d0664cb
Darya Naumenka <naumenka.daria@gmail.com>
2023-02-13 12:34:37 +0300

    fixed conflicts

--- src/by/naumenka/lesson21/Main.java
+++ src/by/naumenka/lesson21/Main.java
@@ -10,29 +10,40 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         ArrayList<Person> people = new ArrayList<>();
-        int ageCount, maleCount, femaleCount;
+        int ageCount;
+        int maleCount;
+        int femaleCount;
         String separator = ",";
 
         System.out.println("Введите ваш путь к файлу");
         String path = new Scanner(System.in).nextLine(); //  src\\document.txt
-        BufferedReader br = new BufferedReader(new FileReader(path));
 
-        String document;
-        String[] infPerson;
-        while ((document = br.readLine()) != null) {
-            infPerson = document.split(separator);
-            people.add(new Person(infPerson[0], infPerson[1], infPerson[2], Integer.parseInt(infPerson[3].trim())));
-            System.out.println(document);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+
+            String document;
+            String[] infPerson;
+            while ((document = br.readLine()) != null) {
+                infPerson = document.split(separator);
+                people.add(new Person(infPerson[0], infPerson[1], infPerson[2], Integer.parseInt(infPerson[3].trim())));
+                System.out.println(document);
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("Не верный путь!");
         }
 
         people.sort(Comparator.comparing(p -> (p.getFirstName() + p.getLastName())));
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(path));
-        for (Person person : people) {
-            bw.write(person.getFirstName() + separator + person.getLastName() + separator +
-                    person.getSex() + separator + person.getAge() + "\n");
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+            for (Person person : people) {
+                bw.write(person.getFirstName() + separator + person.getLastName() + separator +
+                        person.getSex() + separator + person.getAge() + "\n");
+            }
+            bw.close();
+        } catch (IOException ex) {
+            throw new RuntimeException();
         }
-        bw.close();
 
         ageCount = (int) people.stream().filter(age -> age.getAge() > 30).count();
         System.out.println();
